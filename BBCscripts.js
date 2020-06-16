723dc856c7013a66395c8e89d54aea318ec859a4
natewiley11 <43258343+natewiley11@users.noreply.github.com>
2020-06-16 14:20:09 -0400

    Version 4.0
    Final Version of site before upload
    Menu functionality tested
    Broken links removed

--- BBCscripts.js
+++ BBCscripts.js
@@ -2,4 +2,11 @@
 function menu_function() {
     document.getElementById("dropjs").classList.toggle("reveal");
 }
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
 
